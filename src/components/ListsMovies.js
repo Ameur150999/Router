af3754116e7e326f517ed72fc6096d81e0480c6e
Ameur Saoudi <saoudiameur0@gmail.com>
2022-04-16 10:09:45 +0100

    checkpointhooks

---
+++ src/components/ListsMovies.js
@@ -0,0 +1,26 @@
+
+export const ListsMovies =[{
+  id:Math.random(),
+  title:"Avengers",
+  description:"Nick Fury is compelled to launch the Avengers Initiative when Loki poses a threat to planet Earth. His squad of superheroes put their minds together to accomplish the task.",
+  posterUrl:"https://images-na.ssl-images-amazon.com/images/I/719SFBdxRtL._AC_SL1000_.jpg",
+  rate:4
+
+},
+{
+  id:Math.random(),
+  title:"Fast and Furious 8",
+  description:"Dom encounters a mysterious woman, Cipher, who gets him involved in the world of terrorism. The crew has to reunite to stop Cipher and save the man who brought them together as a family.",
+  posterUrl:"https://images-na.ssl-images-amazon.com/images/I/51wUNfZfAuL._AC_.jpg",
+  rate:5
+
+},
+{
+  id:Math.random(),
+  title:"The lion King",
+  description:"Simba, a young lion prince, flees his kingdom after the murder of his father, Mufasa. Years later, a chance encounter with Nala, a lioness, causes him to return and take back what is rightfully his.",
+  posterUrl:"https://images-na.ssl-images-amazon.com/images/I/715DGa4kWVL._AC_SL1000_.jpg",
+  rate:4
+
+}
+]
